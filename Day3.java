74554193c4b10744e02685f18a23e718626780d7
NURASLIAARIFAH <nuraslia617@gmail.com>
2025-09-10 15:27:08 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,19 @@
+package Day3.java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama = "nur aslia arifah";
+        String nim = "D0225023";
+        int umur = 19;
+        // yang diatas kita pake tipe data string dan int serta menambahkan nama variabel
+        System.out.println("nama : " + nama);
+        System.out.println("nim : " + nim);
+        System.out.println("umur : " + umur);
+        //sout untuk menampikan hasil dirun
+        
+        // komentar untuk satu baris 
+      /* diawali dengan  dan diakhiri dengan  berlaku untuk beberapa baris*/
+    }
+    
+}
