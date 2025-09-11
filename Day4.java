342aa1826add99c365132a2a088dece9ef1f14d3
NURASLIAARIFAH <nuraslia617@gmail.com>
2025-09-11 17:18:29 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,22 @@
+package Day4;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Hai \nSemuaa");
+        System.out.println("Perkenalkan nama saya\bNur Aslia Arifah");
+        System.out.println("Saya lahir pada tanggal\t 29 April 2006");
+        System.out.println("Alamat saya \\dipappota\\ belakang kantor pajak majene");
+        System.out.println("Saya Kuliah di \"Universitas Sulawesi Barat\"");
+        System.out.println("Saya mengambil prodi \'informatika\' dengan nim D0225023");
+        
+       // Beberapa Fungsi pengguna Escape Sequence 
+      // \n -> untuk baris baru
+      // \b -> untuk menghapus satu karakter
+      // \t -> untuk memberikan spasi didepan karakter 
+      // \\ -> untuk menampilkan backslash
+      // \" -> untuk menambah tanda kutip pada samping atas kata 
+      // \' -> untuk menambah tanda koma atas pada kata
+    }
+    
+}
