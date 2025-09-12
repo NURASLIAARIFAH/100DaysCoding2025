1b6edd45b859f11019da90288a4455ef1daa5ec5
NURASLIAARIFAH <nuraslia617@gmail.com>
2025-09-12 16:45:35 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,17 @@
+package Day5;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        // Penggunaan tipe data numerik bilangan bulat 
+        int umur = 19;
+        byte anak_ke = 2;
+        long tinggi = 150;
+        short tahun  = 2006;
+        System.out.println("Umur saya saat ini adalah : " + umur);
+        System.out.println("Saya anak ke : " + anak_ke);
+        System.out.println("Tinggi saya saat ini : " + tinggi);
+        System.out.println("Saya lahir pada tahun : " + tahun);
+    }
+    
+}
