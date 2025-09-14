d4d3298f38cc8114b4465c92019e0c6694ed6778
NURASLIAARIFAH <nuraslia617@gmail.com>
2025-09-14 08:48:54 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,15 @@
+package Day7;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        char karakter = 'p';
+        Boolean sehat = true;
+        System.out.println("karakter : " + karakter);
+        System.out.println("sehat : " + sehat);
+        // Tipe data char -> Digunakan untuk karakter tunggal
+        /* Tipe data boolean -> Digunakan untuk merepresentasikan kondisi logis yang hanya memiliki 
+        dua kemungkinan, yaitu True atau False ,benar atau salah */
+    }
+    
+}
