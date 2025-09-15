2062c9099e4017c52664647ec7054989e4f9b127
NURASLIAARIFAH <nuraslia617@gmail.com>
2025-09-15 16:07:49 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,22 @@
+package Day8;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama = "19";
+        String nim = "D0225023";
+        String fakultas = "Teknik";
+        String prodi = "Informatika";
+        String motivasi = "Mau jago bikin aplikasi mobile, makin asik kalo belajarnya lewat Dicoding"; 
+        System.out.println("nama : " + nama);
+        System.out.println("nim : " + nim);
+        System.out.println("fakultas : " + fakultas);
+        System.out.println("prodi : " + prodi);
+        System.out.println("motivasi : " + motivasi);
+        /* String digunakan untuk teks atau kalimat,string juga termasuk tipe data non primitif dan 
+          menggunakan tanda kutip 2 */
+        
+        
+    }
+    
+}
