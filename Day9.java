a07f33b03d14e5fb1ee94306dedb92f34d0c6216
NURASLIAARIFAH <nuraslia617@gmail.com>
2025-09-16 20:30:12 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,20 @@
+package Day9;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama; //Deklarasi variabel adalah proses mendefinisikan variabel dengan menentukan tipe data dan nama variabel
+        int umur; 
+        
+        nama="a'ba"; //Inisialisasi variabel adalah proses memberikan nilai awal kepada variabel yang telah dideklarasikan
+        umur=45;
+        
+        nama="mama"; //Update variabel adalah proses mengubah nilai variabel yang telah ada
+        umur=40;
+        
+        System.out.println(nama);
+        System.out.println(umur);
+        
+        
+    }
+}
