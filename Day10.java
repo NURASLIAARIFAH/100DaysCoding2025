6a72655e54767ac01d125d9fbb381bf44619806b
NURASLIAARIFAH <nuraslia617@gmail.com>
2025-09-17 11:38:10 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,13 @@
+package Day10;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        final int umur =19;
+        System.out.println(umur);
+        /*Constanta (Final Variabel) adalah variabel yang nilainya tidak dapat diubah
+        setelah di inisialisasi.Variabel ini di deklarasikan dengan menggunakan kata kunci final 
+        dan biasanya digunakan untuk menyimpan nilai yang tidak berubah sepanjang program.*/
+    }
+    
+}
