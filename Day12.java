103f527a6ec5f444eb6f46dd249d3901b7e5135f
NURASLIAARIFAH <nuraslia617@gmail.com>
2025-09-19 15:15:09 +0800

    Day12.java

---
+++ Day12.java
@@ -0,0 +1,27 @@
+package Day12;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        // Biodata saya
+        String nama = "Nur Aslia Arifah";
+        String nim = "D0225023";
+        String alamat = "Pappota";
+        int umur = 19;
+        char kelas ='C';
+        Float berat = 53.0f;
+        Double tinggi = 150.0;
+        // Menampilkan output biodata
+        System.out.println("Nama \t: " + nama);
+        System.out.println("Nim \t: " + nim);
+        System.out.println("Alamat \t: " + alamat);
+        System.out.println("Umur \t: " + umur + " tahun");
+        System.out.println("Kelas \t: " + kelas);
+        System.out.println("Berat \t: " + berat);
+        System.out.println("Tinggi \t: " + tinggi);
+        /* Membuat program biodata sederhana dengan menggunakan tipe data
+         * string, integer, char, float, dan double
+         */
+    }
+    
+}
