f3166de3fa32d099c64688e355083b2d4ea59986
NURASLIAARIFAH <nuraslia617@gmail.com>
2025-09-19 22:27:07 +0800

    Day12.java

--- Day12.java
+++ Day12.java
@@ -1,8 +1,9 @@
 package Day12;
-
+import java.util.Scanner;
 public class Main {
     
     public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
         // Biodata saya
         String nama;
         System.out.print("nama:") ;
