44175ede1988d675dec38651ea6448d8235cf92e
NURASLIAARIFAH <nuraslia617@gmail.com>
2025-09-22 20:44:52 +0800

    Day14.java

--- Day14.java
+++ Day14.java
@@ -7,7 +7,7 @@ public class Main {
             
         String Angka = "29";
         int huruf = Integer.parseInt(Angka);
-        System.out.println(Angka);
+        System.out.println(huruf);
        
         String Huruf = "19";
         byte kamu = Byte.parseByte(Huruf);
@@ -23,15 +23,15 @@ public class Main {
          
         String Woman = "1.2";
         double perempuan = Double.parseDouble(Woman);   
-        System.out.println(Woman);
+        System.out.println(perempuan);
         
         String Laptops = "2.3f";
         float laptop = Float.parseFloat(Laptops);
-        System.out.println(Laptops);
+        System.out.println(laptop);
         
         String YoungerSister ="P";
         char adikperempuan = YoungerSister.charAt(0);
-        System.out.println(YoungerSister);
+        System.out.println(adikperempuan);
         
         String Mother = "true";
         boolean ibu = Boolean.parseBoolean(Mother);
