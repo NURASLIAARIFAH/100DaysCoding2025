8e9735e033b41e3e8f5d0a5dd9bf215a9794e6ec
NURASLIAARIFAH <nuraslia617@gmail.com>
2025-09-25 22:03:46 +0800

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,16 @@
+package Day18;
+public class Main {
+    public static void main(String[] args) {
+       //Konversi Otomatis (Dari tipe data kecil ke besar)
+        // int ke long 
+        int nilaiA = 85;
+        long nilaiB = nilaiA;
+        System.out.println("int ke long : " + nilaiB);
+        
+        
+        
+        
+        
+    }
+    
+}
