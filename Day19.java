7704689b6cd598847ed3e4eec3c35537faaf83f8
NURASLIAARIFAH <nuraslia617@gmail.com>
2025-09-26 21:31:21 +0800

    Day19.java

---
+++ Day19.java
@@ -0,0 +1,13 @@
+package Day19;
+public class Main {
+    public static void main(String[] args) {
+        // Konversi manual/paksa (Dari tipe data besar ke kecil)
+         float nilaiA = 23.4f;
+         byte nilaiB = (byte)nilaiA;
+        System.out.println("float ke byte : " + nilaiB);
+        
+        
+        
+    }
+    
+}
