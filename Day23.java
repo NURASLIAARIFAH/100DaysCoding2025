85268fe7f85513ab559e5e6e507adf6ebc12d9a0
NURASLIAARIFAH <nuraslia617@gmail.com>
2025-09-30 19:38:32 +0800

    Day23.java

--- Day23.java
+++ Day23.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
       Scanner s = new Scanner (System.in);
-        System.out.println("panjang : ");
+        System.out.print("panjang : ");
         float panjang = s.nextFloat();
         System.out.print("lebar : ");
         float lebar = s.nextFloat();
