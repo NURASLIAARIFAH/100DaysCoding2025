f0a2ccc06d670e60caeffb4ecf84e6bcd63fb3d5
NURASLIAARIFAH <nuraslia617@gmail.com>
2025-10-02 21:46:23 +0800

    Day25.java

---
+++ Day25.java
@@ -0,0 +1,16 @@
+package Day25;
+public class Main {
+    public static void main(String[] args) {
+       //Operator Increment (++) dan Decrement (--)
+        // Increment
+        int a = 2;
+        System.out.println(a++);
+        System.out.println(++a);
+        
+        // Decrement
+        int b = 4;
+        System.out.println(b--);
+        System.out.println(--b);
+    }
+    
+}
