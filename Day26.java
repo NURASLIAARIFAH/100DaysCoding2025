767c9f5bbdf5f2f57d4f822d60d0a1cd57ea3d1b
NURASLIAARIFAH <nuraslia617@gmail.com>
2025-10-03 20:45:49 +0800

    Day26.java

---
+++ Day26.java
@@ -0,0 +1,19 @@
+package Day26;
+public class Main {
+    public static void main(String[] args) {
+        // Operator Penugasan
+        int a = 4;
+        int b = 2;
+        a += b;
+        System.out.println(a);
+        a -= b;
+        System.out.println(a);
+        a *= b;
+        System.out.println(a);
+        a /= b;
+        System.out.println(a);
+        a %= b;
+        System.out.println(a);
+    }
+    
+}
