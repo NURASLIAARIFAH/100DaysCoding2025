aedacaf474ec61faed77b48f43ce81bb5daf0369
NURASLIAARIFAH <nuraslia617@gmail.com>
2025-10-06 18:54:00 +0800

    Day29.java

---
+++ Day29.java
@@ -0,0 +1,14 @@
+package day29;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Angka1: ");
+        int Angka1 = s.nextInt();
+        System.out.print("Angka2: ");
+        int Angka2 = s.nextInt();
+        System.out.println(Angka1 > Angka2);
+        System.out.println(Angka1 < Angka2);
+    }
+    }
+    
