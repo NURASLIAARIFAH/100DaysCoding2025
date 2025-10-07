334e40d286bfe1800f44b28a1a7b33569bdde50c
NURASLIAARIFAH <nuraslia617@gmail.com>
2025-10-07 19:20:53 +0800

    Day30.java

---
+++ Day30.java
@@ -0,0 +1,14 @@
+package day30;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Angka1: ");
+        int Angka1 = s.nextInt();
+        System.out.print("Angka2: ");
+        int Angka2 = s.nextInt();
+        System.out.println(Angka1 >= Angka2);
+        System.out.println(Angka1 <= Angka2);
+    }
+    }
+    
