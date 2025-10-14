b3f9f66e28a92de63d66f9167da213c5c4dfb823
NURASLIAARIFAH <nuraslia617@gmail.com>
2025-10-14 20:51:29 +0800

    Day37.java

--- Day37.java
+++ Day37.java
@@ -5,9 +5,15 @@ public class Main {
         Scanner s = new Scanner(System.in);
         System.out.print("Angka:");
         int angka = s.nextInt();
-        if(angka % 2 == 0){
-            System.out.println("Genap:");
+        if(angka <= 9){
+            System.out.println("Satuan:");
+        }else if(angka <= 99) 
+            System.out.println("Puluhan:");
+        }else if(angka <= 999) 
+            System.out.println("Ratusan:");
+        }if(angka % 2 == 0){
+            System.out.println("Genap");
         }else{
-            System.out.println("Ganjil:");
+            System.out.println("Ganjil");
         }
         }
