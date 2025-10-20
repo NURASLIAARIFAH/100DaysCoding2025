434bc2c5259d61f430d1086b6e1de4d342088136
NURASLIAARIFAH <nuraslia617@gmail.com>
2025-10-20 20:54:46 +0800

    Day43.java

---
+++ Day43.java
@@ -0,0 +1,34 @@
+package Day43;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+    System.out.print("Masukkan Angka: ");
+        int angka = s.nextInt();
+        String Angka = "";
+        String ganjilGenap = "";
+        String bisaDibagi = "";
+        
+        if(angka <= 9){
+            Angka = "satuan";
+        }else if(angka <= 99){
+            Angka = "puluhan";
+    }else if(angka <= 999){
+        Angka = "ratusan";
+    }else{
+        bisaDibagi = "angka yang tidak habis dibagi";
+    }
+        if(angka % 2 == 0){
+            ganjilGenap = "genap";
+        }else{
+            ganjilGenap = "ganjil";
+        }
+        if(angka % 3 == 0 && angka % 5 == 0){
+            bisaDibagi = "yang bisa dibagi 3 dan 5";
+        }else if(angka % 3 == 0){
+            bisaDibagi = "yang bisa dibagi 3";
+    }else if(angka % 5 == 0){
+        bisaDibagi = "yang bisa dibagi 5";
+    }
+        System.out.println(angka + " adalah angka " + Angka + " " + ganjilGenap + " " + bisaDibagi);
+}}
