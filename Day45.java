1fb4bf94097160d121131a96f656bc58bf235557
NURASLIAARIFAH <nuraslia617@gmail.com>
2025-10-22 21:11:15 +0800

    Day45.java

--- Day45.java
+++ Day45.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.print("Masukkan angka 1-5: ");
-        int angka = s.nextInt();
+        byte angka = s.nextByte();
         switch(angka){
             case 1:
             System.out.println("ini adalah angka satu");
