e8d879fda6ee5781b4e90e0ccc4af6630bf83721
NURASLIAARIFAH <nuraslia617@gmail.com>
2025-10-22 21:03:24 +0800

    Day45.java

---
+++ Day45.java
@@ -0,0 +1,26 @@
+package Day45;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Masukkan angka 1-5: ");
+        int angka = s.nextInt();
+        switch(angka){
+            case 1:
+            System.out.println("ini adalah angka satu");
+            break;
+            case 2:
+            System.out.println("ini adalah angka dua");
+            break;
+            case 3:
+            System.out.println("ini adalah angka tig");
+            break;
+            case 4:
+            System.out.println("ini adalah angka empat");
+            break;
+            case 5:
+            System.out.println("ini adalah angka lima");
+            break;
+            default:
+            System.out.println("angka tidak dikenal");
+        }}}
