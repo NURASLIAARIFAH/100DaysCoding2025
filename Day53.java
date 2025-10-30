52730b49af1456345a3f05d8d78a0976efe53bd7
NURASLIAARIFAH <nuraslia617@gmail.com>
2025-10-30 19:42:26 +0800

    Day53.java

---
+++ Day53.java
@@ -0,0 +1,12 @@
+package Day53;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Masukkan batas: ");
+        int batas = s.nextInt();
+        for(int i = 1; i <= 5; i++){
+                System.out.println(i);
+            if(i == batas){
+                break;
+            }}}}
